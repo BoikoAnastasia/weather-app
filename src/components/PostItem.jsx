283564d832afc8356aa0c6c1e4b1802718e164eb
Anastasia <anastasia151000@rambler.ru>
2022-08-12 23:02:31 +0300

    weather_oneDay

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,19 +1,18 @@
 import React from "react";
+import cl from '../styles/styleComponents.module.css';
 
-
-const PostItem = ({ props, icon }) => {    
-   
-   
-
-    
+const PostItem = ({ props, icon }) => {
     return (
-        <div className="weather">
+        <div >
+            <div className={cl.weather}>
+                {/* Добавить дату, сегодня или завтра */}
+                {/* <p>Температура: <span className="temp">{props.temp}</span>°C</p> */}
+                <p><span className="temp"><img src={icon} alt="" /></span></p>
+                <p><span className="">{props.weather_main}</span></p>
+                
+            </div>
+
             <h4>Погода в городе <span className="city">{props.city}</span></h4>
-            <p>Температура: <span className="temp">{props.temp}</span>°C</p>
-            <p><span className="temp"><img src={icon} alt="" /></span></p>
-            <p>Погода: <span className="temp">{props.weather_main}</span></p>
-            <p>Влажность: <span className="humidity">{props.humidity}</span>%</p>
-            <p>Ветер: <span className="wind">{props.wind}</span> км/ч</p>
         </div>
     )
 }

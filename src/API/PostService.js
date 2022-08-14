77859652268c728bc79e5f5e3b10be1368937d43
Anastasia <anastasia151000@rambler.ru>
2022-08-14 13:27:40 +0300

    а

--- src/API/PostService.js
+++ src/API/PostService.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 export default class PostService{
     static async getAll({city}) {
-        let apiKey = "de24076e8940b80246286f4e77881bbe";
         const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&units=metric&appid=${apiKey}`)
         return response.data;
     }
